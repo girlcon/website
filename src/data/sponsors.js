4fab8fa64980df9ecea77ec4f635ea4fbc2095b2
Ariana Goldstein <goldstein.ariana@gmail.com>
2026-04-16 23:19:39 -0400

    Add sponsor images, make event photos into scrolling carousel

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -3,9 +3,12 @@
 // url: sponsor's website (optional)
 export const sponsors = [
   { name: "Google", logo: "/images/sponsors/google.png", url: "https://google.com" },
-  { name: "CME Group", logo: "/images/sponsors/cme-group.png", url: "https://cmegroup.com" },
-  { name: "ISACA Chicago Chapter", logo: "/images/sponsors/isaca-chicago.png", url: "https://www.isaca.org" },
-  { name: "bitsnbytes cybersecurity", logo: "/images/sponsors/bitsnbytes.png", url: "#" },
-  { name: "AllRise Legal Counsel", logo: "/images/sponsors/allrise.png", url: "#" },
+  { name: "CME Group", logo: "/images/sponsors/cme.png", url: "https://cmegroup.com" },
+  { name: "ISACA Chicago Chapter", logo: "/images/sponsors/isaca.png", url: "https://www.isaca.org" },
+  { name: "Bits 'N Bytes Cybersecurity", logo: "/images/sponsors/bitsnbytes.jpg", url: "https://www.bitsnbytes.us.com/" },
+  { name: "AllRise Legal Counsel", logo: "/images/sponsors/allrise.png", url: "https://www.allriselawyers.com/" },
   { name: "BearWright LLC", logo: "/images/sponsors/bearwright.png", url: "https://bearwrightllc.com" },
+  { name: "Code.org", logo: "/images/sponsors/code.png", url: "https://code.org" },
+  { name: "Exelon", logo: "/images/sponsors/exelon.png", url: "https://www.exeloncorp.com/" },
+  { name: "Intelliguards", logo: "/images/sponsors/intelliguards.png", url: "https://www.intelliguards.com/" },
 ];
