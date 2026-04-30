893bd79fd44131b48bffb477ce084883bc917a6b
Ariana Goldstein <goldstein.ariana@gmail.com>
2026-04-29 22:03:24 -0400

    Add high school names, fix core team layout

--- src/data/core-team.js
+++ src/data/core-team.js
@@ -2,15 +2,15 @@
 // photo: path relative to /public/images/core-team/
 // Leave photo as "" to show a placeholder avatar.
 export const coreTeam = [
-  { name: "Deeya Pendharkar",  role: "Co-Director",      school: "High School Name", photo: "/images/core-team/deeya-pendharkar.jpg" },
-  { name: "Laasya Manikonda",  role: "Co-Director",      school: "High School Name", photo: "/images/core-team/laasya-manikonda.jpg" },
-  { name: "Alison Fung",       role: "Core Team Member", school: "High School Name", photo: "/images/core-team/alison-fung.jpg" },
-  { name: "Anoushka Sandeep",  role: "Core Team Member", school: "High School Name", photo: "/images/core-team/anoushka-sandeep.jpg" },
-  { name: "Ayesha Khan",       role: "Core Team Member", school: "High School Name", photo: "/images/core-team/ayesha-khan.jpg" },
-  { name: "Krithika Bayyapu",  role: "Core Team Member", school: "High School Name", photo: "/images/core-team/krithika-bayyapu.jpg" },
-  { name: "Nisha Salian",      role: "Core Team Member", school: "High School Name", photo: "/images/core-team/nisha-salian.jpg" },
-  { name: "Saanvika Chikoti",  role: "Core Team Member", school: "High School Name", photo: "/images/core-team/saanvika-chikoti.jpg" },
-  { name: "Sophia Zheng",      role: "Core Team Member", school: "High School Name", photo: "/images/core-team/sophia-zheng.jpg" },
-  { name: "Vivian Hong",       role: "Core Team Member", school: "High School Name", photo: "/images/core-team/vivian-hong.jpg" },
-  { name: "Zahra Moosani",     role: "Core Team Member", school: "High School Name", photo: "/images/core-team/zahra-moosani.jpg" },
+  { name: "Deeya Pendharkar",  role: "Co-Director",      school: "Adlai E. Stevenson High School", photo: "/images/core-team/deeya-pendharkar.jpg" },
+  { name: "Laasya Manikonda",  role: "Co-Director",      school: "Vernon Hills High School", photo: "/images/core-team/laasya-manikonda.jpg" },
+  { name: "Alison Fung",       role: "Core Team Member", school: "Illinois Mathematics and Science Academy", photo: "/images/core-team/alison-fung.jpg" },
+  { name: "Anoushka Sandeep",  role: "Core Team Member", school: "Waubonsie Valley High School", photo: "/images/core-team/anoushka-sandeep.jpg" },
+  { name: "Ayesha Khan",       role: "Core Team Member", school: "Indiana Academy for Science, Math, and Humanities", photo: "/images/core-team/ayesha-khan.jpg" },
+  { name: "Krithika Bayyapu",  role: "Core Team Member", school: "Adlai E. Stevenson High School", photo: "/images/core-team/krithika-bayyapu.jpg" },
+  { name: "Nisha Salian",      role: "Core Team Member", school: "Adlai E. Stevenson High School", photo: "/images/core-team/nisha-salian.jpg" },
+  { name: "Saanvika Chikoti",  role: "Core Team Member", school: "Adlai E. Stevenson High School", photo: "/images/core-team/saanvika-chikoti.jpg" },
+  { name: "Sophia Zheng",      role: "Core Team Member", school: "University of Chicago Laboratory Schools", photo: "/images/core-team/sophia-zheng.jpg" },
+  { name: "Vivian Hong",       role: "Core Team Member", school: "DePaul College Prep", photo: "/images/core-team/vivian-hong.jpg" },
+  { name: "Zahra Moosani",     role: "Core Team Member", school: "Naperville Central High School", photo: "/images/core-team/zahra-moosani.jpg" },
 ];
